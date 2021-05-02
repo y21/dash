28ea693fc9048ab7d2ec4d9db150c3d74898f428
y21 <btimo8822@gmail.com>
2021-05-02 13:05:54 +0200

    util & visitor

---
+++ src/util.rs
@@ -0,0 +1,14 @@
+pub fn is_digit(c: u8) -> bool {
+    (b'0'..b'9').contains(&c)
+}
+
+pub fn is_alpha(c: u8) -> bool {
+    (b'a'..b'z').contains(&c) ||
+        (b'A'..b'Z').contains(&c) ||
+        c == b'_'
+}
+
+/// Checks if `c` is a valid character for the start of an identifier
+pub fn is_identifier_start(c: u8) -> bool {
+    is_alpha(c)
+}
