c56ecbab4f29aaa8eefe64ba1d4e9c8cfe658b60
y21 <btimo8822@gmail.com>
2021-05-08 15:12:08 +0200

    cli prototype

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,26 @@
-use clap::{App, SubCommand};
+use std::{fs, path::PathBuf};
 
-fn main() {
-    let args = App::new("Dash")
-        .about("Execute JavaScript code using the JavaScript engine dash")
-        .subcommand(SubCommand::with_name("run"))
-        .get_matches();
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "dash")]
+struct Args {
+    #[structopt(name = "file", parse(from_os_str))]
+    file: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Args::from_args();
+    let file = opt
+        .file
+        .to_str()
+        .expect("Failed to parse file input string");
+
+    let code = fs::read_to_string(file)?;
+
+    if let Err(e) = dash::eval(code) {
+        println!("{:?}", e);
+    }
+
+    Ok(())
 }
