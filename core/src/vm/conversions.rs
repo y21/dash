6fbc23d59de27a9cf23625d143611283371e3279
y21 <btimo8822@gmail.com>
2021-05-11 21:32:45 +0200

    refactor: move value conversions to separate file

---
+++ core/src/vm/conversions.rs
@@ -0,0 +1,31 @@
+use super::value::*;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl From<NativeFunction> for Rc<RefCell<Value>> {
+    fn from(f: NativeFunction) -> Self {
+        Rc::new(RefCell::new(Value::new(ValueKind::Object(Box::new(
+            Object::Function(FunctionKind::Native(f)),
+        )))))
+    }
+}
+
+impl From<Value> for Rc<RefCell<Value>> {
+    fn from(v: Value) -> Self {
+        Rc::new(RefCell::new(v))
+    }
+}
+
+impl From<Object> for Value {
+    fn from(o: Object) -> Self {
+        Self::new(ValueKind::Object(Box::new(o)))
+    }
+}
+
+impl From<UserFunction> for Value {
+    fn from(f: UserFunction) -> Self {
+        Self::new(ValueKind::Object(Box::new(Object::Function(
+            FunctionKind::User(f),
+        ))))
+    }
+}
