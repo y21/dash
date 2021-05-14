13ba2dd9fcf7ea9227fefc907c6c71ca6e634265
y21 <btimo8822@gmail.com>
2021-05-14 23:22:29 +0200

    make ScopeGuard generic

--- core/src/compiler/scope.rs
+++ core/src/compiler/scope.rs
@@ -13,22 +13,14 @@ impl<'a> Local<'a> {
 }
 
 #[derive(Debug)]
-pub struct ScopeGuard<'a, const N: usize> {
+pub struct ScopeGuard<T, const N: usize> {
     pub depth: u16,
-    locals: Stack<Local<'a>, N>,
+    locals: Stack<T, N>,
 }
 
-impl<'a, const N: usize> ScopeGuard<'a, N> {
-    pub fn new() -> Self {
-        Self {
-            locals: Stack::new(),
-            depth: 0,
-        }
-    }
-
+impl<'a, const N: usize> ScopeGuard<Local<'a>, N> {
     pub fn find_variable(&self, name: &'a [u8]) -> Option<usize> {
         let depth = self.depth;
-        // TODO: take scope depth into account
         self.locals
             .find(|local| {
                 local.depth <= depth && local.ident.len() == name.len() && local.ident.eq(name)
@@ -36,13 +28,22 @@ impl<'a, const N: usize> ScopeGuard<'a, N> {
             .map(|(idx, _)| idx)
     }
 
-    pub fn local_count(&self) -> usize {
-        self.locals.len()
-    }
-
     pub fn push_local(&mut self, local: Local<'a>) -> usize {
         self.locals.push(local);
         self.local_count() - 1
+    }
+}
+
+impl<T, const N: usize> ScopeGuard<T, N> {
+    pub fn new() -> Self {
+        Self {
+            locals: Stack::new(),
+            depth: 0,
+        }
+    }
+
+    pub fn local_count(&self) -> usize {
+        self.locals.len()
     }
 
     pub fn enter_scope(&mut self) {
