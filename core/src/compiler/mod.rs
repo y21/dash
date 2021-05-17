bbd509885f35ef92c002ca44dd2cc7802918c988
y21 <btimo8822@gmail.com>
2021-05-17 23:50:04 +0200

    remove random tests

--- core/src/compiler/mod.rs
+++ core/src/compiler/mod.rs
@@ -1,32 +1,3 @@
 pub mod compiler;
 pub mod scope;
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        parser::{lexer::Lexer, parser::Parser},
-        vm::{
-            instruction::{Instruction, Opcode},
-            value::Value,
-            VM,
-        },
-    };
-
-    use super::compiler::Compiler;
-
-    #[test]
-    pub fn compiler() {
-        let src = r#"let a=3+1; a+1"#;
-
-        let tokens = Lexer::new(src).scan_all();
-
-        let statements = Parser::new(tokens).parse_all();
-
-        let instructions = Compiler::new(statements).compile();
-
-        let mut vm = VM::new(instructions);
-        vm.interpret().unwrap();
-
-        vm.stack.dump();
-    }
-}
+pub mod upvalue;
