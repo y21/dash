19c637fa5d7df1231efa645644e3354b4719a1b6
y21 <btimo8822@gmail.com>
2021-05-24 01:14:52 +0200

    is_numeric helper function

--- core/src/util.rs
+++ core/src/util.rs
@@ -10,3 +10,7 @@ pub fn is_alpha(c: u8) -> bool {
 pub fn is_identifier_start(c: u8) -> bool {
     is_alpha(c)
 }
+
+pub fn is_numeric(c: impl AsRef<str>) -> bool {
+    c.as_ref().chars().all(|c| c.is_numeric())
+}
