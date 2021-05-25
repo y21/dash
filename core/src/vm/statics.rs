027363fbe40205c8ea3e8f599ea5bb98da4b048e
y21 <btimo8822@gmail.com>
2021-05-26 00:35:19 +0200

    more sane statics implementation

--- core/src/vm/statics.rs
+++ core/src/vm/statics.rs
@@ -5,87 +5,20 @@ use crate::js_std;
 use super::value::function::NativeFunction;
 use super::value::Value;
 
-pub mod id {
-    pub const CONSOLE_LOG: usize = 0;
-    pub const ISNAN: usize = 1;
-    pub const ARRAY_PUSH: usize = 2;
-    pub const MATH_POW: usize = 3;
+pub struct Statics {
+    pub console_log: Rc<RefCell<Value>>,
+    pub isnan: Rc<RefCell<Value>>,
+    pub array_push: Rc<RefCell<Value>>,
+    pub math_pow: Rc<RefCell<Value>>,
 }
-
-const ID_COUNT: usize = 4;
-
-/// Static values
-pub struct Statics([MaybeUninit<Rc<RefCell<Value>>>; ID_COUNT]);
 
 impl Statics {
     pub fn new() -> Self {
-        let mut statics = unsafe { Self(MaybeUninit::uninit().assume_init()) };
-        statics.prepare();
-        statics
-    }
-
-    fn prepare(&mut self) {
-        self.set(
-            id::ISNAN,
-            NativeFunction::new("isNaN", js_std::functions::is_nan, None).into(),
-        );
-
-        self.set(
-            id::MATH_POW,
-            NativeFunction::new("pow", js_std::math::pow, None).into(),
-        );
-
-        self.set(
-            id::ARRAY_PUSH,
-            NativeFunction::new("push", js_std::array::push, None).into(),
-        );
-
-        self.set(
-            id::CONSOLE_LOG,
-            NativeFunction::new("log", js_std::console::log, None).into(),
-        );
-    }
-
-    pub fn set(&mut self, id: usize, value: Rc<RefCell<Value>>) {
-        self.0[id] = MaybeUninit::new(value);
-    }
-
-    pub unsafe fn get_unchecked(&self, id: usize) -> &Rc<RefCell<Value>> {
-        &*self.0[id].as_ptr()
-    }
-
-    pub unsafe fn get_mut_unchecked(&mut self, id: usize) -> &mut Rc<RefCell<Value>> {
-        &mut *self.0[id].as_mut_ptr()
-    }
-
-    pub unsafe fn iter_unchecked(&self) -> StaticsIter<'_> {
-        StaticsIter(self, 0)
-    }
-}
-
-impl Drop for Statics {
-    fn drop(&mut self) {
-        for idx in 0..ID_COUNT {
-            unsafe {
-                std::ptr::drop_in_place(self.0[idx].as_mut_ptr());
-            }
+        Self {
+            console_log: NativeFunction::new("log", js_std::console::log, None).into(),
+            isnan: NativeFunction::new("isNaN", js_std::functions::is_nan, None).into(),
+            array_push: NativeFunction::new("push", js_std::array::push, None).into(),
+            math_pow: NativeFunction::new("pow", js_std::math::pow, None).into(),
         }
-    }
-}
-
-pub struct StaticsIter<'a>(&'a Statics, usize);
-impl<'a> Iterator for StaticsIter<'a> {
-    type Item = Rc<RefCell<Value>>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.1 >= ID_COUNT {
-            return None;
-        }
-
-        let value = unsafe { self.0.get_unchecked(self.1) };
-
-        self.1 += 1;
-
-        Some(value.clone())
     }
 }
