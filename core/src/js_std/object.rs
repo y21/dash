9349d16377b3bbf3947bcfafa939c7b2e442756f
y21 <btimo8822@gmail.com>
2021-05-26 18:51:46 +0200

    std: Object.defineProperty, Object.getOwnPropertyNames

---
+++ core/src/js_std/object.rs
@@ -0,0 +1,34 @@
+use std::{cell::RefCell, rc::Rc};
+
+use crate::vm::value::{array::Array, function::CallContext, object::Object, Value, ValueKind};
+
+pub fn define_property(value: CallContext) -> Rc<RefCell<Value>> {
+    let mut arguments = value.arguments();
+
+    let obj_cell = arguments.next().unwrap();
+    let mut obj = obj_cell.borrow_mut();
+    let prop_cell = arguments.next().unwrap();
+    let prop = prop_cell.borrow();
+    let prop_str = prop.as_string_lossy().unwrap();
+    let descriptor_cell = arguments.next().unwrap();
+
+    let value = Value::get_property(descriptor_cell, "value").unwrap();
+    obj.set_property(&*prop_str, value);
+
+    obj_cell.clone()
+}
+
+pub fn get_own_property_names(value: CallContext) -> Rc<RefCell<Value>> {
+    let obj_cell = value.args.first().unwrap();
+    let obj = obj_cell.borrow();
+
+    let mut keys = Vec::with_capacity(obj.fields.len());
+    for key in obj.fields.keys() {
+        let key: &str = &*key;
+        keys.push(Rc::new(RefCell::new(Value::new(ValueKind::Object(
+            Box::new(Object::String(String::from(key))),
+        )))));
+    }
+
+    Value::new(ValueKind::Object(Box::new(Object::Array(Array::new(keys))))).into()
+}
