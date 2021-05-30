8f4b6d5dbc131890cb7bd706a4b16e29413b1a68
y21 <btimo8822@gmail.com>
2021-05-30 03:55:07 +0200

    frame: unwind handler & loop comments

--- core/src/vm/frame.rs
+++ core/src/vm/frame.rs
@@ -12,7 +12,18 @@ pub struct Frame {
 
 #[derive(Debug)]
 pub struct UnwindHandler {
+    /// Catch block instruction pointer
     pub catch_ip: usize,
+    /// Catch error value
     pub catch_value_sp: Option<usize>,
+    /// Finally block instruction pointer
     pub finally_ip: Option<usize>,
+}
+
+#[derive(Debug)]
+pub struct Loop {
+    /// Loop condition instruction pointer
+    pub condition_ip: usize,
+    /// End of loop instruction pointer
+    pub end_ip: usize,
 }
