fa08cf3ccc13494043c8a9e36b35c29f3697d55b
y21 <btimo8822@gmail.com>
2021-06-01 22:57:55 +0200

    std: string constructor

--- core/src/js_std/string.rs
+++ core/src/js_std/string.rs
@@ -1,7 +1,11 @@
-use crate::vm::value::{function::CallContext, Value};
+use crate::vm::value::{function::CallContext, Value, ValueKind};
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub fn index_of(args: CallContext) -> Rc<RefCell<Value>> {
-    todo!()
+pub fn string_constructor(_args: CallContext) -> Result<Rc<RefCell<Value>>, Rc<RefCell<Value>>> {
+    Ok(Value::new(ValueKind::Undefined).into())
+}
+
+pub fn index_of(_args: CallContext) -> Result<Rc<RefCell<Value>>, Rc<RefCell<Value>>> {
+    Ok(Value::new(ValueKind::Undefined).into())
 }
