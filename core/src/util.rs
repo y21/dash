f9fa7ca90cf071af15112218c84e9150f67d0c8e
y21 <btimo8822@gmail.com>
2021-06-07 23:57:52 +0200

    add MaybeOwned util

--- core/src/util.rs
+++ core/src/util.rs
@@ -1,3 +1,5 @@
+use std::ptr::NonNull;
+
 pub fn is_digit(c: u8) -> bool {
     (b'0'..=b'9').contains(&c)
 }
@@ -13,4 +15,37 @@ pub fn is_identifier_start(c: u8) -> bool {
 
 pub fn is_numeric(c: impl AsRef<str>) -> bool {
     c.as_ref().chars().all(|c| c.is_numeric())
+}
+
+#[derive(Debug, Clone)]
+pub enum MaybeOwned<T> {
+    Owned(T),
+    Borrowed(*mut T),
+}
+
+impl<T> MaybeOwned<T> {
+    pub fn as_ptr(&mut self) -> *mut T {
+        match self {
+            Self::Owned(v) => v as *mut T,
+            Self::Borrowed(v) => *v,
+        }
+    }
+
+    pub fn as_borrowed(&mut self) -> Self {
+        Self::Borrowed(self.as_ptr())
+    }
+
+    pub unsafe fn as_ref(&self) -> &T {
+        match self {
+            Self::Borrowed(b) => &**b,
+            Self::Owned(b) => b,
+        }
+    }
+
+    pub unsafe fn as_mut(&mut self) -> &mut T {
+        match self {
+            Self::Borrowed(b) => &mut **b,
+            Self::Owned(b) => b,
+        }
+    }
 }
