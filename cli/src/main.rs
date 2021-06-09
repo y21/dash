fdf0ff390aa0450fc008a4f0b78b6c9888680fde
y21 <btimo8822@gmail.com>
2021-06-09 23:47:19 +0200

    cli: repl support

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,26 +1,66 @@
-use std::{fs, path::PathBuf};
+use std::{
+    borrow::Cow,
+    cell::RefCell,
+    fs,
+    io::{self, Write},
+    path::PathBuf,
+};
 
+use dash::{compiler::agent::Agent, vm::value::Value};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "dash")]
 struct Args {
     #[structopt(name = "file", parse(from_os_str))]
-    file: PathBuf,
+    file: Option<PathBuf>,
+}
+
+fn create_agent() -> impl Agent {
+    runtime::agent(runtime::agent_flags::FS)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Args::from_args();
-    let file = opt
-        .file
-        .to_str()
-        .expect("Failed to parse file input string");
 
-    let code = fs::read_to_string(file)?;
+    if let Some(file) = &opt.file {
+        let file = file.to_str().expect("Failed to parse file input string");
 
-    if let Err(e) = dash::eval(code) {
-        println!("{:?}", e);
+        let code = fs::read_to_string(file)?;
+
+        if let Err(e) = dash::eval(&code, Some(create_agent())) {
+            println!("{:?}", e);
+        }
+    } else {
+        repl();
     }
 
     Ok(())
+}
+
+fn repl() {
+    println!("Welcome to the dash REPL\nType JavaScript code and hit enter to evaluate it");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let s = &mut String::new();
+        io::stdin().read_line(s).expect("Failed to read line");
+
+        match dash::eval(s, Some(create_agent())) {
+            Ok(result) => {
+                let result_ref = result.as_deref().map(RefCell::borrow);
+                let result_fmt = result_ref
+                    .as_deref()
+                    .map(Value::inspect)
+                    .unwrap_or(Cow::Borrowed("undefined"));
+
+                println!("{}", result_fmt);
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        };
+    }
 }
