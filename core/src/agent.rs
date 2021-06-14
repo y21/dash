5eb7d8f790b820c8569b45fb85ce50a7a5f13030
y21 <btimo8822@gmail.com>
2021-06-14 18:33:53 +0200

    agent: make methods optional to implement

--- core/src/agent.rs
+++ core/src/agent.rs
@@ -7,17 +7,13 @@ pub enum ImportResult {
 
 pub trait Agent {
     /// A method that is called when the compiler resolves an import statement
-    fn import(&mut self, module_name: &[u8]) -> Option<ImportResult>;
-    /// A method that is called at runtime when Math.random() is called
-    fn random(&mut self) -> Option<f64>;
-}
-
-impl Agent for () {
-    fn import(&mut self, _: &[u8]) -> Option<ImportResult> {
+    fn import(&mut self, module_name: &[u8]) -> Option<ImportResult> {
         None
     }
-
+    /// A method that is called at runtime when Math.random() is called
     fn random(&mut self) -> Option<f64> {
         None
     }
 }
+
+impl Agent for () {}
