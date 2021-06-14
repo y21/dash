752a5a58d76c12ae59e6237e50026f8e73598aaf
y21 <btimo8822@gmail.com>
2021-06-14 05:04:35 +0200

    agent: add random method

--- core/src/agent.rs
+++ core/src/agent.rs
@@ -6,11 +6,18 @@ pub enum ImportResult {
 }
 
 pub trait Agent {
+    /// A method that is called when the compiler resolves an import statement
     fn import(&mut self, module_name: &[u8]) -> Option<ImportResult>;
+    /// A method that is called at runtime when Math.random() is called
+    fn random(&mut self) -> Option<f64>;
 }
 
 impl Agent for () {
     fn import(&mut self, _: &[u8]) -> Option<ImportResult> {
+        None
+    }
+
+    fn random(&mut self) -> Option<f64> {
         None
     }
 }
