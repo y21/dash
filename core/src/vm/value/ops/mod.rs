15e2f5e720f61f30ca1fb041a1836aa0d19b5b7a
y21 <btimo8822@gmail.com>
2022-02-20 11:35:55 +0100

    redo vm

--- core/src/vm/value/ops/mod.rs
+++
@@ -1,8 +0,0 @@
-/// Assignment operations on JavaScript values
-pub mod assign;
-/// Binary operations on JavaScript values
-pub mod binary;
-/// Compare operations on JavaScript values
-pub mod compare;
-/// Logical operations on JavaScript values
-pub mod logic;
