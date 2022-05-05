64da1b353f111db9733a9223788935851fc3df14
y21 <btimo8822@gmail.com>
2022-05-06 01:35:53 +0200

    handle rt::eval errors

--- dash-cli/src/cmd/run.rs
+++ dash-cli/src/cmd/run.rs
@@ -1,3 +1,4 @@
+use dash::optimizer::consteval::OptLevel;
 use dash_core as dash;
 use dash_rt::runtime::Runtime;
 use dash_rt::state::State;
@@ -19,22 +20,26 @@ pub fn run(args: &ArgMatches) -> anyhow::Result<()> {
     let before = args.is_present("timing").then(|| Instant::now());
 
     let async_rt = tokio::runtime::Runtime::new()?;
-    async_rt.block_on(async move {
-        let mut rt = Runtime::new().await;
+    async_rt.block_on(inner(source, opt))?;
 
-        let value = rt.eval(&source, opt).unwrap();
-        let mut sc = LocalScope::new(rt.vm_mut());
-        println!("{}", value.to_string(&mut sc).unwrap());
+    if let Some(before) = before {
+        println!("{:?}", before.elapsed());
+    }
 
-        if let Some(before) = before {
-            println!("{:?}", before.elapsed());
-        }
+    Ok(())
+}
 
-        let state = State::try_from_vm(rt.vm()).unwrap();
-        if state.needs_event_loop() {
-            rt.run_event_loop().await;
-        }
-    });
+async fn inner(source: String, opt: OptLevel) -> anyhow::Result<()> {
+    let mut rt = Runtime::new().await;
+
+    let value = rt.eval(&source, opt).map_err(|e| anyhow::anyhow!("{e}"))?;
+    let mut sc = LocalScope::new(rt.vm_mut());
+    println!("{}", value.to_string(&mut sc).unwrap());
+
+    let state = State::try_from_vm(rt.vm()).unwrap();
+    if state.needs_event_loop() {
+        rt.run_event_loop().await;
+    }
 
     Ok(())
 }
