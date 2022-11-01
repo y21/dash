21890696048d403c2cce2e39e4af6d0b9ce16b9e
y21 <30553356+y21@users.noreply.github.com>
2022-11-01 02:10:21 +0100

    rt: detect import cycles

--- crates/dash_rt_script_modules/src/lib.rs
+++ crates/dash_rt_script_modules/src/lib.rs
@@ -1,21 +1,52 @@
+use std::cell::RefCell;
+
 use dash_middle::compiler::StaticImportKind;
 use dash_rt::module::ModuleLoader;
 use dash_vm::local::LocalScope;
 use dash_vm::throw;
 use dash_vm::value::Value;
 use dash_vm::Vm;
+use indexmap::IndexSet;
 
 #[derive(Debug)]
-pub struct ScriptModule;
+pub struct ScriptModule {
+    import_stack: RefCell<IndexSet<String>>,
+}
+
+impl ScriptModule {
+    pub fn new() -> Self {
+        Self {
+            import_stack: RefCell::new(IndexSet::new()),
+        }
+    }
+
+    pub fn add_import(&self, sc: &mut LocalScope, name: &str) -> Result<(), Value> {
+        let mut stack = self.import_stack.borrow_mut();
+        if stack.contains(name) {
+            throw!(sc, "import cycle detected: {}", name);
+        }
+
+        stack.insert(name.to_string());
+        Ok(())
+    }
+
+    pub fn pop_import(&self) {
+        self.import_stack.borrow_mut().pop();
+    }
+}
 
 impl ModuleLoader for ScriptModule {
     fn import(&self, sc: &mut LocalScope, import_ty: StaticImportKind, path: &str) -> Result<Option<Value>, Value> {
+        self.add_import(sc, path)?;
+
         let contents = match std::fs::read_to_string(path) {
             Ok(c) => c,
             Err(err) => throw!(sc, "{}", err),
         };
-        let module = Vm::evaluate_module(sc, &contents, import_ty, Default::default())?;
+        let module = Vm::evaluate_module(sc, &contents, import_ty, Default::default());
 
-        Ok(Some(module))
+        self.pop_import();
+
+        module.map(Some)
     }
 }
