da6fc150fc7170245e23e144153287be688a949a
y21 <30553356+y21@users.noreply.github.com>
2022-12-21 21:12:48 +0100

    std: basic timer api

--- crates/dash_rt_modules/src/lib.rs
+++ crates/dash_rt_modules/src/lib.rs
@@ -4,6 +4,7 @@ pub fn init_modules() -> Box<dyn ModuleLoader> {
     let module = dash_rt_http::HttpModule
         .or(dash_rt_fs::FsModule)
         .or(dash_rt_fetch::FetchModule)
+        .or(dash_rt_timers::TimersModule::new())
         .or(dash_rt_script_modules::ScriptModule::new());
 
     Box::new(module)
---
+++ crates/dash_rt_timers/src/lib.rs
@@ -0,0 +1,81 @@
+use std::sync::Arc;
+use std::time::Duration;
+
+use dash_middle::compiler::StaticImportKind;
+use dash_middle::util::ThreadSafeStorage;
+use dash_rt::event::EventMessage;
+use dash_rt::module::ModuleLoader;
+use dash_rt::state::State;
+use dash_vm::gc::persistent::Persistent;
+use dash_vm::local::LocalScope;
+use dash_vm::throw;
+use dash_vm::value::function::native::CallContext;
+use dash_vm::value::function::Function;
+use dash_vm::value::function::FunctionKind;
+use dash_vm::value::object::NamedObject;
+use dash_vm::value::object::Object;
+use dash_vm::value::object::PropertyValue;
+use dash_vm::value::ops::abstractions::conversions::ValueConversion;
+use dash_vm::value::Value;
+
+#[derive(Debug)]
+pub struct TimersModule;
+
+impl TimersModule {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl ModuleLoader for TimersModule {
+    fn import(&self, sc: &mut LocalScope, _import_ty: StaticImportKind, path: &str) -> Result<Option<Value>, Value> {
+        if path == "@std/timers" {
+            let obj = NamedObject::new(sc);
+
+            let set_timeout = Function::new(sc, Some("setTimeout".into()), FunctionKind::Native(set_timeout));
+            let set_timeout = Value::Object(sc.register(set_timeout));
+
+            obj.set_property(sc, "setTimeout".into(), PropertyValue::static_default(set_timeout))?;
+
+            Ok(Some(Value::Object(sc.register(obj))))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+fn set_timeout(cx: CallContext) -> Result<Value, Value> {
+    let callback = match cx.args.first() {
+        Some(Value::Object(cb)) => cb.clone(),
+        _ => throw!(cx.scope, "missing callback function argument"),
+    };
+
+    let callback = Arc::new(ThreadSafeStorage::new(Persistent::new(callback)));
+
+    let delay = match cx.args.get(1) {
+        Some(delay) => delay.to_int32(cx.scope)? as u64,
+        None => throw!(cx.scope, "Missing delay argument"),
+    };
+
+    let state = State::from_vm(cx.scope);
+    let tx = state.event_sender();
+    let tid = state.active_tasks().add();
+
+    state.rt_handle().spawn(async move {
+        let tx2 = tx.clone();
+        tokio::time::sleep(Duration::from_millis(delay)).await;
+
+        tx.send(EventMessage::ScheduleCallback(Box::new(move |rt| {
+            let mut sc = LocalScope::new(rt.vm_mut());
+            let callback = callback.get();
+
+            if let Err(err) = callback.apply(&mut sc, Value::undefined(), Vec::new()) {
+                eprintln!("Unhandled error in timer callback: {:?}", err);
+            }
+
+            tx2.send(EventMessage::RemoveTask(tid));
+        })));
+    });
+
+    Ok(Value::undefined())
+}
