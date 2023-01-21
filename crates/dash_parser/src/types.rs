00641b4e37979732a2b8ab973a8fc54828177d3a
y21 <30553356+y21@users.noreply.github.com>
2023-01-21 18:57:33 +0100

    fix incorrect generic precedence in type parser

--- crates/dash_parser/src/types.rs
+++ crates/dash_parser/src/types.rs
@@ -9,9 +9,6 @@ use crate::Parser;
 pub trait TypeParser<'a> {
     fn parse_type_segment(&mut self) -> Option<TypeSegment<'a>>;
 
-    /// Parses a generic type: foo<bar>
-    fn parse_generic_type(&mut self) -> Option<TypeSegment<'a>>;
-
     /// Parses a union type: foo | bar
     fn parse_union_type(&mut self) -> Option<TypeSegment<'a>>;
 
@@ -21,17 +18,52 @@ pub trait TypeParser<'a> {
     /// Parse postfix array notation: foo[], foo[][], foo[][][], ...
     fn parse_postfix_array(&mut self) -> Option<TypeSegment<'a>>;
 
+    /// Parses a generic type: foo<bar>
+    fn parse_generic_type(&mut self) -> Option<TypeSegment<'a>>;
+
     /// Parses a primary type: literals (true, false, Uint8Array)
     fn parse_primary_type(&mut self) -> Option<TypeSegment<'a>>;
 }
 
 impl<'a> TypeParser<'a> for Parser<'a> {
     fn parse_type_segment(&mut self) -> Option<TypeSegment<'a>> {
-        self.parse_generic_type()
+        self.parse_union_type()
+    }
+
+    fn parse_union_type(&mut self) -> Option<TypeSegment<'a>> {
+        let mut left = self.parse_intersection_type()?;
+
+        while self.expect_and_skip(&[TokenType::BitwiseOr], false) {
+            let right = self.parse_intersection_type()?;
+            left = TypeSegment::Union(Box::new(left), Box::new(right));
+        }
+
+        Some(left)
+    }
+
+    fn parse_intersection_type(&mut self) -> Option<TypeSegment<'a>> {
+        let mut left = self.parse_postfix_array()?;
+
+        while self.expect_and_skip(&[TokenType::BitwiseAnd], false) {
+            let right = self.parse_postfix_array()?;
+            left = TypeSegment::Union(Box::new(left), Box::new(right));
+        }
+
+        Some(left)
+    }
+
+    fn parse_postfix_array(&mut self) -> Option<TypeSegment<'a>> {
+        let mut target = self.parse_generic_type()?;
+
+        while self.expect_and_skip(&[TokenType::EmptySquareBrace], false) {
+            target = TypeSegment::Array(Box::new(target));
+        }
+
+        Some(target)
     }
 
     fn parse_generic_type(&mut self) -> Option<TypeSegment<'a>> {
-        let mut left = self.parse_union_type()?;
+        let mut left = self.parse_primary_type()?;
 
         while self.expect_and_skip(&[TokenType::Less], false) {
             let mut args = Vec::new();
@@ -49,38 +81,6 @@ impl<'a> TypeParser<'a> for Parser<'a> {
         }
 
         Some(left)
-    }
-
-    fn parse_union_type(&mut self) -> Option<TypeSegment<'a>> {
-        let mut left = self.parse_intersection_type()?;
-
-        while self.expect_and_skip(&[TokenType::BitwiseOr], false) {
-            let right = self.parse_intersection_type()?;
-            left = TypeSegment::Union(Box::new(left), Box::new(right));
-        }
-
-        Some(left)
-    }
-
-    fn parse_intersection_type(&mut self) -> Option<TypeSegment<'a>> {
-        let mut left = self.parse_postfix_array()?;
-
-        while self.expect_and_skip(&[TokenType::BitwiseOr], false) {
-            let right = self.parse_postfix_array()?;
-            left = TypeSegment::Union(Box::new(left), Box::new(right));
-        }
-
-        Some(left)
-    }
-
-    fn parse_postfix_array(&mut self) -> Option<TypeSegment<'a>> {
-        let mut target = self.parse_primary_type()?;
-
-        while self.expect_and_skip(&[TokenType::EmptySquareBrace], false) {
-            target = TypeSegment::Array(Box::new(target));
-        }
-
-        Some(target)
     }
 
     fn parse_primary_type(&mut self) -> Option<TypeSegment<'a>> {
