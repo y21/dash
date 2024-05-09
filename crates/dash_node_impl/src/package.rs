60593f657cb41e7668723e3f342106cfb2573434
y21 <30553356+y21@users.noreply.github.com>
2024-05-09 20:33:40 +0200

    resolve directory requires correctly

--- crates/dash_node_impl/src/package.rs
+++ crates/dash_node_impl/src/package.rs
@@ -4,11 +4,16 @@ use std::path::PathBuf;
 use dash_proc_macro::Trace;
 use serde::Deserialize;
 
+fn default_main() -> PathBuf {
+    "index.js".into()
+}
+
 #[derive(Deserialize, Debug, Trace)]
 pub struct Package {
     pub name: String,
     pub version: String,
     pub description: String,
+    #[serde(default = "default_main")]
     pub main: PathBuf,
     #[serde(default)]
     pub dependencies: HashMap<String, String>,
