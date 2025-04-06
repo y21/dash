4ee8266e500245912c1b85ea1980deb24798dbca
y21 <30553356+y21@users.noreply.github.com>
2025-04-06 22:58:00 +0200

    implement `require('fs').promises.writeFile`

--- crates/dash_rt_fs/src/promises.rs
+++ crates/dash_rt_fs/src/promises.rs
@@ -1,22 +1,32 @@
+use std::cell::Cell;
+use std::path::PathBuf;
+
 use dash_rt::wrap_async;
 use dash_vm::localscope::LocalScope;
+use dash_vm::throw;
 use dash_vm::value::error::Error;
-use dash_vm::value::function::native::CallContext;
-use dash_vm::value::function::{Function, FunctionKind};
+use dash_vm::value::function::native::{CallContext, register_native_fn};
 use dash_vm::value::object::{NamedObject, Object, PropertyValue};
 use dash_vm::value::ops::conversions::ValueConversion;
 use dash_vm::value::propertykey::ToPropertyKey;
+use dash_vm::value::typedarray::TypedArray;
 use dash_vm::value::{Value, ValueContext};
 
 pub fn init_module(sc: &mut LocalScope) -> Result<Value, Value> {
-    let name = sc.intern("readFile");
-    let read_file_value = Function::new(sc, Some(name.into()), FunctionKind::Native(read_file));
-    let read_file_value = sc.register(read_file_value);
+    let read_file_k = sc.intern("readFile");
+    let write_file_k = sc.intern("writeFile");
+    let read_file_value = register_native_fn(sc, read_file_k, read_file);
+    let write_file_value = register_native_fn(sc, write_file_k, write_file);
 
     let module = NamedObject::new(sc);
     module.set_property(
-        name.to_key(sc),
-        PropertyValue::static_default(Value::object(read_file_value)),
+        read_file_k.to_key(sc),
+        PropertyValue::static_default(read_file_value.into()),
+        sc,
+    )?;
+    module.set_property(
+        write_file_k.to_key(sc),
+        PropertyValue::static_default(write_file_value.into()),
         sc,
     )?;
 
@@ -39,4 +49,35 @@ fn read_file(cx: CallContext) -> Result<Value, Value> {
             Err(Value::object(sc.register(err)))
         }
     })
+}
+
+fn write_file(cx: CallContext) -> Result<Value, Value> {
+    let [path, buf] = *cx.args else {
+        throw!(
+            cx.scope,
+            Error,
+            "Invalid arguments passed to fs.writeFileSync(path, buf)"
+        )
+    };
+    let path = path.to_js_string(cx.scope)?;
+    if let Some(array) = buf.extract::<TypedArray>(cx.scope) {
+        let path = PathBuf::from(path.res(cx.scope));
+
+        let storage = array
+            .arraybuffer(cx.scope)
+            .storage()
+            .iter()
+            .map(Cell::get)
+            .collect::<Vec<_>>();
+
+        wrap_async(cx, tokio::fs::write(path, storage), |sc, res| match res {
+            Ok(()) => Ok(Value::undefined()),
+            Err(err) => {
+                let err = Error::new(sc, err.to_string());
+                Err(Value::object(sc.register(err)))
+            }
+        })
+    } else {
+        throw!(cx.scope, TypeError, "Invalid source passed to fs.writeFileSync")
+    }
 }
